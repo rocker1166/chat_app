00843133e89dbaa367d2590c5e7eaf283951b8ce
SUMAN JANA <sumanjanaled@gmail.com>
2024-07-30 23:37:58 +0530

    new route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/index.html', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
